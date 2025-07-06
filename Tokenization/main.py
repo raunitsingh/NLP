7dc9bbfba693744c314309ce11bfb73eac53315a
raunitsingh <raunitsingh33@gmail.com>
2025-07-06 23:21:57 +0530

    Added Tokenization File

--- Tokenization/main.py
+++ Tokenization/main.py
@@ -1,13 +1,12 @@
 # pip install nltk spacy
-# python -m spacy download en_core_web_sm
-
+# python -m spacy download en_core_web_sm   
 # Import libraries
-import nltk
+import nltk                                            #Natural language  toolkit
 from nltk.tokenize import word_tokenize, sent_tokenize
-import spacy
+import spacy                                           #modern NLP library
 
 # Download both punkt and punkt space
-nltk.download('')
+nltk.download('punkt')
 
 # Sample text
 text = "I love NLP! It's fun and exciting."
@@ -24,10 +23,27 @@ sentences = sent_tokenize(text)
 print(sentences)
 
 # spaCy: Word and Sentence Tokenization
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")             #(like a mini brain that can understand eng)
 
-doc = nlp(text)
+doc = nlp(text)                                #processes the string and returns a doc object (spacy pipeline - tokeinzation, tagging and recognition)
 print("\nspaCy Word Tokenization:")
 print([token.text for token in doc])
 print("\nspaCy Sentence Tokenization:")
 print([sent.text for sent in doc.sents])
+
+
+
+
+#OUTPUT:
+
+# NLTK Word Tokenization:
+#['I', 'love', 'NLP', '!', 'It', "'s", 'fun', 'and', 'exciting', '.']
+
+#NLTK Sentence Tokenization:
+#['I love NLP!', "It's fun and exciting."]
+
+#spaCy Word Tokenization:
+#['I', 'love', 'NLP', '!', 'It', "'s", 'fun', 'and', 'exciting', '.']
+
+#spaCy Sentence Tokenization:
+#['I love NLP!', "It's fun and exciting."]
